80756af53d820673d286424a1f3060947ab6a9a3
tbw <145964518+wTatiya@users.noreply.github.com>
2025-10-27 13:48:44 +0700

    Update urlpatterns to include admin path

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+
 
 def index(request):
     return render(request, 'index.html')
 
-urlpatterns = [path('', index)]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', lambda request: render(request, 'index.html')),
+]
