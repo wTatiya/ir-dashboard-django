55e2c92364ff08819f7ec64b3f195c954889dbb9
tbw <145964518+wTatiya@users.noreply.github.com>
2025-10-27 13:47:52 +0700

    Update settings for Django application configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,10 +1,23 @@
-SECRET_KEY = '9f4ac7a0d54142a03913b1d95bea142b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '9f4ac7a0d54142a03913b1d95bea142b')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = ['django.contrib.staticfiles']
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
 ROOT_URLCONF = 'mysite.urls'
-MIDDLEWARE = []
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',]
 
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
